50c4d3eaf6c00164bbc3c2b448f06d1d672002c8
Bill <119852615+skopelosbill@users.noreply.github.com>
2026-05-10 06:30:02 +0300

    Fix formatting of final answer in 05.js

--- problems/gcse/2026/05.js
+++ problems/gcse/2026/05.js
@@ -232,7 +232,7 @@ const problemBank = [
             "Region $A$ only contains $\\{5, 7\\}$.",
             "Region $B$ only contains $\\{1, 4, 6\\}$.",
             "Outside: $\\{8, 9, 10\\}$.",
-            "Final Answer: $$\\frac{2}{10} \\text{ (or } 0.2) <br><img src='images/05/g_10_sol.png' alt='Venn Solution' style='max-width:300px;'>$$"
+            "Final Answer: $$\\frac{2}{10} \\text{ (or } 0.2)<img src='images/05/g_10_sol.png' alt='Venn Solution' style='max-width:300px;'>$$"
         ],
         "bradley_insight": {
             "type": "deeper",
